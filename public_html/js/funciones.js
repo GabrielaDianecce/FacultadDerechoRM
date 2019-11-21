80f1b97ec9534953177fd6a0616e2bef49f07963
GabrielaDianecce <gabrieladianecce@gmailcom>
2019-11-21 17:54:03 -0300

    Site RM

---
+++ public_html/js/funciones.js
@@ -0,0 +1,87 @@
+
+function muestra_oculta() {
+    if (document.getElementById) {
+        var el = document.getElementById("c_01");
+        console.log("aqui ee", el);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+    }
+}
+
+function muestra_oculta_2() {
+    if (document.getElementById) {
+        var el = document.getElementById("c_02");
+        console.log("aqui ee", el);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+    }
+}
+function muestra_oculta_3() {
+    if (document.getElementById) {
+        var el = document.getElementById("c_03");
+        console.log("aqui ee", el);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+    }
+}
+function muestra_oculta_4() {
+    if (document.getElementById) {
+        var el = document.getElementById("c_04");
+        console.log("aqui ee", el);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+    }
+}
+function muestra_oculta_5() {
+    if (document.getElementById) {
+        var el = document.getElementById("c_05");
+        console.log("aqui ee", el);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+    }
+}
+function muestra_oculta_6() {
+    if (document.getElementById) {
+        var el = document.getElementById("c_06");
+        console.log("aqui ee", el);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+    }
+}
+function muestra_oculta_7() {
+    if (document.getElementById) {
+        var el = document.getElementById("c_07");
+        console.log("aqui ee", el);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+    }
+}
+function muestra_oculta_8() {
+    if (document.getElementById) {
+        var el = document.getElementById("c_08");
+        console.log("aqui ee", el);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+    }
+}
+function muestra_oculta_9() {
+    if (document.getElementById) {
+        var el = document.getElementById("c_09");
+        console.log("aqui ee", el);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+    }
+}
+function muestra_oculta_10() {
+    if (document.getElementById) {
+        var el = document.getElementById("c_10");
+        console.log("aqui ee", el);
+        el.style.display = (el.style.display == 'none') ? 'block' : 'none';
+    }
+}
+
+
+window.onload = function () {
+    muestra_oculta();
+    muestra_oculta_2();
+    muestra_oculta_3();
+    muestra_oculta_4();
+    muestra_oculta_5();
+    muestra_oculta_6();
+    muestra_oculta_7();
+    muestra_oculta_8();
+    muestra_oculta_9();
+    muestra_oculta_10();
+}
+console.log(c);
